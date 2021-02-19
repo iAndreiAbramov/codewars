0e808165f3c9d2b56a8f9256f988791558d176f0
Andrei <i.andrei.abramov@gmail.com>
2021-02-20 02:07:00 +0300

    20.02.2021

---
+++ Century_From_Year.js
@@ -0,0 +1,15 @@
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+
+// Task :
+// Given a year, return the century it is in.
+
+function century(year) {
+  return Math.ceil(year / 100);
+}
+
+console.log(century(1705), 18, 'Testing for year 1705');
+console.log(century(1900), 19, 'Testing for year 1900');
+console.log(century(1601), 17, 'Testing for year 1601');
+console.log(century(2000), 20, 'Testing for year 2000');
+console.log(century(89), 1, 'Testing for year 89');
---
+++ Sort_by_Last_Char.js
@@ -0,0 +1,17 @@
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+// If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+// All inputs will be valid.
+
+function last(x) {
+  const arr = x.split(' ')
+  const sortedArr = []
+
+  arr.forEach(item => {
+    item
+  })
+  return sortedArr
+}
+
+console.log(last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']);
+console.log(last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']); 
+console.log(last('take me to semynak'), ['take', 'me', 'semynak', 'to']);
