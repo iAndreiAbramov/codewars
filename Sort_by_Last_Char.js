688dcc11cc410fc3e2bcc2846f7b7bf1daa23362
iAndreiAbramov <70846277+iAndreiAbramov@users.noreply.github.com>
2021-02-22 12:23:48 +0300

    Update Sort_by_Last_Char.js

--- Sort_by_Last_Char.js
+++ Sort_by_Last_Char.js
@@ -2,14 +2,13 @@
 // If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
 // All inputs will be valid.
 
-// Short solution
+// Alter solution
 
 // function last(x){
 //   return x.split(' ').sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1));
 // }
 
 // My solution )))
-
 
 function last(x) {
   return arrOfWords = x.split(' ').sort((a, b) => a.slice(-1).localeCompare(b.slice(-1)))
