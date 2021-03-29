2e05263f1f7f23a770c6031a44dd7a28b4260871
Andrei <i.andrei.abramov@gmail.com>
2021-03-29 22:20:24 +0300

    29.03.2021

--- 6_Title_Case.js
+++ 6_Title_Case.js
@@ -2,16 +2,20 @@
 // Write a function that will convert a string into title case, given an optional list of exceptions (minor words). The list of minor words will be given as a string with each word separated by a space. Your function should ignore the case of the minor words string -- it should behave in the same way even if the case of the minor word string is changed.
 
 function titleCase(title, minorWords = '') {
-  return title.split(' ').map(word => {
-    if (!minorWords.toLowerCase().split().includes(word.toLowerCase())) {
-      return word.toLowerCase().replace(/\b[a-z]/, char => char.toUpperCase());
+  let result = title.toLowerCase().replace(/\b[a-z]/, char => char.toUpperCase());
+  result = result.split(' ').map(word => {
+    if (!minorWords.toLowerCase().split(' ').includes(word.toLowerCase())) {
+      return word.replace(/\b[a-z]/, char => char.toUpperCase());
     } else {
-      return word.toLowerCase();
+      return word;
     }
-  }).join(' ').trim().replace(/\b[a-z]/, char => char.toUpperCase());
+  }).join(' ').trim();
+  return result;
 }
 
 console.log(titleCase(''), '');
 console.log(titleCase('a clash of KINGS', 'a an the of'), 'A Clash of Kings')
 console.log(titleCase('THE WIND IN THE WILLOWS', 'The In'), 'The Wind in the Willows')
 console.log(titleCase('the quick brown fox'), 'The Quick Brown Fox')
+console.log(titleCase('a bc', 'bc'), 'A bc')
+console.log(titleCase('a bc', 'BC'), 'A bc')
