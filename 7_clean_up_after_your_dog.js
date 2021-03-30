095ee37bcb95e3bd3af8f49568c6657cb9814592
Andrei <i.andrei.abramov@gmail.com>
2021-03-31 01:12:51 +0300

    30.03.2021

--- 7_clean_up_after_your_dog.js
+++ 7_clean_up_after_your_dog.js
@@ -5,30 +5,50 @@
 // If you do, return 'Clean', else return 'Cr@p'.
 // Watch out though - if your dog is out there ('D'), he gets very touchy about being watched. If he is there you need to return 'Dog!!'.
 
-function crap(x, bags, cap){
-  if (x.filter(item => item.includes('D')).length > 0) return 'Dog!!'
+// function crap(x, bags, cap){
+//   if (x.filter(item => item.includes('D')).length > 0) return 'Dog!!'
 
-  const f = x => Array.isArray(x) ? Math.max(...x.map(f)) + 1 : 0
+//   const f = x => Array.isArray(x) ? Math.max(...x.map(f)) + 1 : 0
 
-  const crapArr = x.filter(item => item.includes('@'))
-  if (crapArr.length > bags && f(x) > 1) return "Cr@p";
-  if (crapArr.length > 1 && f(x) < 1 && bags < 1) return "Cr@p";
+//   const crapArr = x.filter(item => item.includes('@'))
+//   if (crapArr.length > bags && f(x) > 1) return "Cr@p";
+//   if (crapArr.length > 1 && f(x) < 1 && bags < 1) return "Cr@p";
 
-  const crapAmount = [];
-  let crapMax;
-  if (f(x) > 1) {
-    crapArr.forEach(item => {
-      crapAmount.push(item.reduce((sum, cur) => (cur === '@') ? sum += 1 : sum, 0))
-    })
-    crapMax = Math.max(...crapAmount)
-  } else {
-    crapMax = crapArr.length
+//   const crapAmount = [];
+//   let crapMax;
+//   if (f(x) > 1) {
+//     crapArr.forEach(item => {
+//       crapAmount.push(item.reduce((sum, cur) => (cur === '@') ? sum += 1 : sum, 0))
+//     })
+//     crapMax = Math.max(...crapAmount)
+//   } else {
+//     crapMax = crapArr.length
+//   }
+//   if (crapMax > cap) return "Cr@p"
+//   return 'Clean'
+// }
+
+
+
+function crap(x, bags, cap) {
+  const overallCap = bags * cap;
+
+  if (x.some(subArray => subArray.includes('D'))) return 'Dog!!';
+
+  let crapCounter = 0;
+
+  for (let i = 0; i < x.length; i++) {
+    for (let j = 0; j < x[i].length; j++) {
+      if (x[i][j] === '@') crapCounter++;
+      if (crapCounter > overallCap) return 'Cr@p';
+    }
   }
-  if (crapMax > cap) return "Cr@p"
-  return 'Clean'
+
+  return 'Clean';
 }
 
-// console.log(crap([['_','_','_','_'], ['_','_','_','@'], ['_','_','@', '_']], 2, 2), "Clean");
-// console.log(crap([['_','_','_','_'], ['_','_','_','@'], ['_','_','@', '_']], 1, 1), "Cr@p");
-// console.log(crap([['_','_'], ['_','@'], ['D','_']], 2, 2), "Dog!!");
+
+console.log(crap([['_','_','_','_'], ['_','_','_','@'], ['_','_','@', '_']], 2, 2), "Clean");
+console.log(crap([['_','_','_','_'], ['_','_','_','@'], ['_','_','@', '_']], 1, 1), "Cr@p");
+console.log(crap([['_','_'], ['_','@'], ['D','_']], 2, 2), "Dog!!");
 console.log(crap(['_','_','_','@','_','_','_','_','_','_','_','_','@','_','_','_','_','_'], 1, 5), "Clean");
